0a3f76dce7398406c9033217518cfbd22b7a69d4
William P. <williampratama837@gmail.com>
2026-01-02 03:12:10 +0700

    fix: all error page structure

--- webApp/resources/views/errors/500.blade.php
+++ webApp/resources/views/errors/500.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>500 Internal Server Error</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class='bg-primary-100 h-screen flex items-center justify-center mx-8'>
-    <div class=' flex flex-col w-full h-max items-center md:flex-row md:justify-center'>
-        <div class='w-72 mb-1 md:w-80'>
-            <img src="{{ asset("assets/500.png") }}" alt="404">
-        </div>
-        <div class='font-secondaryAndButton text-small text-white text-center md:text-justify md:ml-8 md:text-body-size'>
-            <p class='font-primary text-body-size font-bold text-warning-lighten mb-1 md:text-paragraph'>500 | Internal Server Error</p>
-            <p>Oops, something went wrong. Try to refresh this page or feel free to contact us if the problem persists.</p>
-        </div>
-    </div>
-</body>
-</html>
+@extends('layouts.error', [
+    'errorCode' => '500',
+    'errorIdentity' => 'Internal Server Error',
+    'errorDescription' => 'Oops, something went wrong. Try to refresh this page or feel free to contact us if the problem persists.'
+])
--- webApp/resources/views/errors/503.blade.php
+++ webApp/resources/views/errors/503.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>503 Service Unavailable</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class='bg-primary-100 h-screen flex items-center justify-center mx-8'>
-    <div class=' flex flex-col w-full h-max items-center lg:flex-row lg:justify-center'>
-        <div class='w-72 mb-1 md:w-80'>
-            <img src="{{ asset("assets/503.png") }}" alt="503">
-        </div>
-        <div class='font-secondaryAndButton text-small text-white text-center lg:text-justify lg:ml-8 lg:text-body-size'>
-            <p class='font-primary font-bold text-warning-lighten text-body-size mb-1 lg:text-paragraph'>503 | Service Unavailable</p>
-            <p>Website is under construction or maintenance. Sorry for the inconvenient. We will back soon!</p>
-        </div>
-    </div>
-</body>
-</html>
+@extends('layouts.error', [
+    'errorCode' => '503',
+    'errorIdentity' => 'Service Unavailable',
+    'errorDescription' => 'Website is under construction or maintenance. Sorry for the inconvenient. We will back soon!'
+])
