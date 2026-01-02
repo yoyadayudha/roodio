8a31e65e333aa9eb013b20ed6504a942530bb34d
William P. <williampratama837@gmail.com>
2026-01-02 15:04:01 +0700

    add: button component

--- webApp/resources/views/components/button.blade.php
+++ webApp/resources/views/components/button.blade.php
@@ -1,26 +1,64 @@
-{{-- <div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
-</div>
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
-<div>
-    <!-- We must ship. - Taylor Otwell -->
-</div> --}}
+@props([
+  'behaviour' => 'navigation', //action OR navigation
+  'actionType' => 'button',
+  'navLink' => "{{ route('welcome') }}",
+  'content' => 'hallo ini button',
+  'mood' => null //happy, sad, relaxed, angry
+])
 
-{{-- @props([
-  'type' => 'button',
-  'variant' => 'primary'
-]) --}}
+@php
+    $baseStyle = 'relative overflow-hidden w-max px-4 py-1 font-secondaryAndButton rounded-3xl cursor-pointer text-md text-center font-bold before:absolute before:inset-0 before:-translate-x-full before:transition-transform before:duration-300 hover:before:translate-x-0 md:text-smallBtn md:px-5 md:py-1.5';
+    
+    // 100, 85, 70, 60, 50, 30, 20, 10
 
-{{-- <button
-  type="{{ $type }}"
-  {{ $attributes->merge([
-    'class' => "px-4 py-2 rounded text-white bg-{$variant}"
-  ]) }}
->
-  {{ $slot }}
-</button> --}}
+    $defaultStyle = 'bg-primary-10 text-primary-85 before:bg-primary-50 hover:text-white';
+    $happyMood = 'bg-secondary-happy-20 text-secondary-happy-100 before:bg-secondary-happy-85 hover:text-secondary-happy-10';
+    $sadMood = 'bg-secondary-sad-20 text-secondary-sad-100 before:bg-secondary-sad-85 hover:text-secondary-sad-10';
+    $relaxedMood = 'bg-secondary-relaxed-20 text-secondary-relaxed-100 before:bg-secondary-relaxed-85 hover:text-secondary-relaxed-10';
+    $angryMood = 'bg-secondary-angry-20 text-secondary-angry-100 before:bg-secondary-angry-85 hover:text-secondary-angry-10';
 
+    switch ($mood) {
+      case 'happy':
+        $conditionalStyle = $happyMood;
+        break;
+      
+      case 'sad':
+        $conditionalStyle = $sadMood;
+        break;
+      
+      case 'relaxed':
+        $conditionalStyle = $relaxedMood;
+        break;
+      
+      case 'angry':
+        $conditionalStyle = $angryMood;
+        break;
+      
+      default:
+        $conditionalStyle = $defaultStyle;
+        break;
+    }
 
-{{-- <x-ui.button variant="secondary">
-  Submit
-</x-ui.button> --}}
+@endphp
+
+<body class='bg-primary-100'>
+  
+  @if ($behaviour == 'action')
+      <button 
+        type='{{ $actionType }}' 
+        {{ $attributes->merge([
+          'class' => $baseStyle . ' ' . $conditionalStyle
+        ]) }}
+      >
+        <span class='relative z-5'>{{ $content }}</span>
+      </button>
+  @else
+      <a href="{{ $navLink }}">
+        <div {{ $attributes->merge(['class' => $baseStyle . ' ' . $conditionalStyle]) }}>
+          <span class='relative z-5'>{{ $content }}</span>
+        </div>
+      </a>
+  @endif
+</body>
